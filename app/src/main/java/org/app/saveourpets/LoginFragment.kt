48231df97ee2f37c3dd5fd6ff415c31ab919bf7e
Ernest00 <ernesto.anaya2014@gmail.com>
2023-04-14 16:48:42 -0600

    Diseño Navegación y Menú

--- app/src/main/java/org/app/saveourpets/LoginFragment.kt
+++ app/src/main/java/org/app/saveourpets/LoginFragment.kt
@@ -1,19 +1,31 @@
 package org.app.saveourpets
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 
 class LoginFragment : Fragment() {
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val button = view.findViewById<Button>(R.id.btn_login)
+        button.setOnClickListener {
+            val intent = Intent(activity, AdminActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        return view
     }
+
 }
--- app/src/main/java/org/app/saveourpets/MainActivity.kt
+++ app/src/main/java/org/app/saveourpets/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
